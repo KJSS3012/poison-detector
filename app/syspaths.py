16ec7e40dd9c7fc8b6750f2fa23507172b9961a2
Daniel macedo <enosmacedo@gmail.com>
2025-11-03 18:46:48 -0300

    refactor: adding a enum estructure

---
+++ app/syspaths.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class SysPaths(Enum):
+    PATH_BASE_DATASET = "./base_data_set/"
+    PATH_CLIENT_MODELS = "./clients/models/"
+    PATH_CENTRAL_MODELS = "./central/models/"
