6ff4ba7e966250a7cf40b7080a4a6b0c6b0b2a0e
Daniel macedo <enosmacedo@gmail.com>
2025-11-04 19:19:05 -0300

    feat: method to get acc of models to analyses

---
+++ app/analyses/view_acc.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn.functional as F
+from torchvision import datasets
+from modelNet import Net, netTransform
+from sysvars import SysVars as svar
+
+def test(model, data_path=svar.PATH_BASE_DATASET):
+    """
+    Method to test a model with MNIST dataset.
+
+    args:
+    - model: the weights in static dict format of the model to be tested.
+    - device: the device to run the testing on ('cpu' or 'cuda').
+    - data_path: path to the dataset location.
+    """
+    
+    device = svar.DEFAULT_DEVICE.value
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(data_path, download=False, train=False, transform=netTransform),
+        batch_size=64, shuffle=True, **{})
+    
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
