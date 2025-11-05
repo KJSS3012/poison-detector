035c95781d0862f8e3c9595e841c0d1a0d1677a3
Daniel macedo <enosmacedo@gmail.com>
2025-11-04 21:50:24 -0300

    feat: Analyses interface logic and creating a poisoned dataset

--- app/controler.py
+++ app/controler.py
@@ -1,8 +1,10 @@
 import os
 from clients.train import post_train as client_train
 from central.train import post_train as central_train
+from analyses.get_confusion_map import get_confusion_map
+from analyses.view_acc import get_accuracy
 from torchvision import datasets, transforms
-from modelNet import netTransform
+from modelNet import netTransform, Net
 from torch.utils.data import random_split
 from sysvars import SysVars as svar
 import pandas as pd
@@ -15,7 +17,8 @@ def post_client_train():
     Method to simulate a client training session.
     It's call the post_train method from clients/train.py with some default parameters.
     """
-    client_train(epochs=5, load_data=True, data_path=svar.PATH_BASE_DATASET)
+    client_train(epochs=100, load_data=True, data_path=svar.PATH_BASE_DATASET.value)
+    client_train(epochs=100, load_data=True, data_path="./datasets/poisoned_data_set_1/")
 
 
 
@@ -62,8 +65,8 @@ def get_weights(isCentral = True,selected_indice_models: list = []):
     if selected_indice_models == []:
         for model_name in models_path:
             model_path = path + model_name
-            model_static_dict = torch.load(model_path)
-            selected_models_dict[model_name] = model_static_dict
+            state_dict = torch.load(model_path)
+            selected_models_dict[model_name] = state_dict
 
     elif selected_indice_models == [-1]:
         model_path = path + model_path[-1]
@@ -75,23 +78,66 @@ def get_weights(isCentral = True,selected_indice_models: list = []):
             if sel_model in models_indices:
                 model_name = "model_" + str(sel_model) + ".pt"
                 model_path = path + model_name
-                model_static_dict = torch.load(model_path)
-                selected_models_dict[model_name] = model_static_dict
+                state_dict = torch.load(model_path)
+                selected_models_dict[model_name] = state_dict
 
     return selected_models_dict
     
-
 
 
 def get_analyses():
     # Carregar modelo (ou modelos) e fazer testes
     # Salvar testes em CSV
     #Seguindo esse modelo
-    data = {
-        "Index": ["Value1", "Value2"]
-    }
-    df = pd.DataFrame(data)
-    df.to_csv("data.csv", index=False, encoding="utf-8")
+
+    #if not os.path.exists(svar.PATH_ANALYSES_CVS.value + "analyses.csv"):
+    #    train_labels = {
+    #        "train_id": [],
+    #        "accuracy": [],
+    #        "benign_clients": [],
+    #        "malignant_clients": [],
+    #        "poisoning": []
+    #    }
+    #    train_table = pd.DataFrame(train_labels)
+    
+    #else: train_table = pd.read_csv(svar.PATH_ANALYSES_CVS.value + "analyses.csv")
+
+    if not os.path.exists(svar.PATH_ANALYSES_CVS.value + "map.csv"):
+        map_labels = {
+            "train_id": [],
+            "0": [],
+            "1": [],
+            "2": [],
+            "3": [],
+            "4": [],
+            "5": [],
+            "6": [],
+            "7": [],
+            "8": [],
+            "9": []
+        }
+        map_table = pd.DataFrame(map_labels)
+
+    else: map_table = pd.read_csv(svar.PATH_ANALYSES_CVS.value + "map.csv")
+
+
+
+    #### BUILD ANALYSES HERE ####
+    models = get_weights(isCentral=False, selected_indice_models=[4, 5])
+
+    #get_confusion_map(models["model_4.pt"], "model_4")
+    #get_confusion_map(models["model_5.pt"], "model_5")
+    acc_belign = get_accuracy(models["model_4.pt"])
+    acc_malign = get_accuracy(models["model_5.pt"])
+
+    print("Acc benign client model 4: ", acc_belign)
+    print("Acc malign client model 5: ", acc_malign)
+
+
+
+
+    #train_table.to_csv(svar.PATH_ANALYSES_CVS.value + "analyses.csv", index=False, encoding="utf-8")
+    #map_table.to_csv(svar.PATH_ANALYSES_CVS.value + "map.csv", index=False, encoding="utf-8")
     ...
 
 
@@ -118,10 +164,14 @@ def manipule_data():
     # Carregue apenas N% dos dados
     # 
 
-    mnist_trainset = datasets.MNIST(root='./data', train=True, download=False, transform=netTransform)
+    mnist_trainset = datasets.MNIST(root=svar.PATH_BASE_DATASET.value, train=True, download=False, transform=netTransform)
 
 
     # AQUI POSSO ALTERAR O MNIST TRAINSET
+    for i in range(len(mnist_trainset)):
+        t, c = mnist_trainset[i]
+        if c == 7:
+            mnist_trainset.targets[i] = 1
 
 
     total_size = len(mnist_trainset)
@@ -132,16 +182,14 @@ def manipule_data():
     train_indices = train_dataset.indices
     test_indices = test_dataset.indices
     
-    train_images = mnist_trainset.data[train_indices].clone()
-    train_images[0]
-    train_labels = mnist_trainset.targets[train_indices].clone()
-    train_labels[0]
+    train_data = mnist_trainset.data[train_indices].clone()
+    train_targets = mnist_trainset.targets[train_indices].clone()
 
-    test_images = mnist_trainset.data[test_indices].clone()
-    test_labels = mnist_trainset.targets[test_indices].clone()
+    test_data = mnist_trainset.data[test_indices].clone()
+    test_targets = mnist_trainset.targets[test_indices].clone()
 
 
     #os.mkdir("./data/MNIST/processed")
-    torch.save((train_images,train_labels), './data/MNIST/processed/training.pt')
-    torch.save((test_images,test_labels), './data/MNIST/processed/test.pt')
+    torch.save((train_data, train_targets), './datasets/poisoned_data_set_1/MNIST/processed/training.pt')
+    torch.save((test_data, test_targets), './datasets/poisoned_data_set_1/MNIST/processed/test.pt')
     ...
