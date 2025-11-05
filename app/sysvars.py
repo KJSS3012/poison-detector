e419e92f15094bd0f982435ec9b41af4ecf52ec7
Daniel macedo <enosmacedo@gmail.com>
2025-11-04 21:50:52 -0300

    refactor: Adding a analyses paths

--- app/sysvars.py
+++ app/sysvars.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
 class SysVars(Enum):
-    PATH_BASE_DATASET = "./base_data_set/"
+    DEFAULT_DEVICE = "cuda"
+    PATH_BASE_DATASET = "./datasets/base_data_set/"
     PATH_CLIENT_MODELS = "./clients/models/"
     PATH_CENTRAL_MODELS = "./central/models/"
-    DEFAULT_DEVICE = "cuda"
+    PATH_ANALYSES_GRAPHICS = "./analyses/graphics/"
+    PATH_ANALYSES_CVS = "./analyses/csv/"
