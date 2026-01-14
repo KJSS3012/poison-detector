d1a7bc81aee3443e4a0916e5566094ca41d5327f
Daniel macedo <enosmacedo@gmail.com>
2026-01-14 13:37:51 -0300

    feat: add default paths in sysvars

--- sysvars.py
+++ sysvars.py
@@ -1,15 +1,35 @@
-from enum import Enum
+from pathlib import Path
 
-class SysVars(Enum):
+class SysVars:
     """
-    System Variables used across the project
+    Centralized experiment-aware paths
     """
+
+    ROOT = Path.cwd()
+
     DEFAULT_DEVICE = "cuda"
-    PATH_BASE_DATASET = "./datasets/base_data_set/"
-    PATH_CLIENT_MODELS = "./clients/models/"
-    PATH_CENTRAL_MODELS = "./central/models/"
-    PATH_ANALYSES_GRAPHICS = "./analyses/graphics/"
-    PATH_ANALYSES_CVS = "./analyses/csv/"
-    PATH_GRADCAMS = "./analyses/gradcams/"
-    PATH_LIMES = "./analyses/limes/"
-    MODELS_INFO = "./clients/models_info.json"
+
+    EXPERIMENT_NAME = "experiment_002"
+    EXPERIMENT_ROOT = ROOT / "experiments" / EXPERIMENT_NAME
+
+    EXPERIMENT_CSV = EXPERIMENT_ROOT / "csv"
+    EXPERIMENT_DATASETS = EXPERIMENT_ROOT / "datasets"
+    EXPERIMENT_GRAPHICS = EXPERIMENT_ROOT / "imgs" / "graphics"
+    EXPERIMENT_GRAD_CAMS = EXPERIMENT_ROOT / "imgs" / "gradCams"
+    EXPERIMENT_MODELS = EXPERIMENT_ROOT / "models"
+
+    TESTE_ROOT_PATH = ROOT / "experiments" / "test"
+
+    DATASETS_ROOT = ROOT / "datasets"
+
+    MNIST_TRAIN_PATH = DATASETS_ROOT / "mnist" / "training.pt"
+    MNIST_TEST_PATH = DATASETS_ROOT / "mnist" / "test.pt"
+    MNIST_ROOT_PATH = DATASETS_ROOT / "mnist"
+
+    EMNIST_TRAIN_PATH = DATASETS_ROOT / "emnist" / "training.pt"
+    EMNIST_TEST_PATH = DATASETS_ROOT / "emnist" / "test.pt"
+    EMNIST_ROOT_PATH = DATASETS_ROOT / "emnist"
+
+    SAMPLE_IMAGES_PATH = ROOT / "services" / "xai" / "sample_images"
+
+    
